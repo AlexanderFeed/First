051e19638f599c9dc7ee6bf2e987b5ba74dc5a3d
Александр Николаевич Мищенко <alex1mishchenko@gmail.com>
2022-03-11 18:47:07 +0300

    1.3

--- 1.3/Program.fs
+++ 1.3/Program.fs
@@ -2,7 +2,63 @@
 
 open System
 
+//снизу вверх
+let rec mul x =
+    if x = 0 then 1
+    else x%10 * mul(x/10)
+
+    //сверху вниз
+let mul2 x =
+    let rec sup x cur =
+        if x = 0 then cur
+        else 
+            let new_cur = x%10 * cur ////////////////?????????????
+            let new_x = x/10
+            sup new_x new_cur
+    sup x 1
+
+//снизу вверх min
+let mutable m = 10
+let rec min x =
+    if(x=0) then m
+    else
+        if(x%10 < m) then m <- x%10
+        min(x/10)
+//сверху вниз
+let min2 x =
+    let rec mup x cur =
+        if x=0 then cur
+        else    
+            let new_cur = if x%10 < cur then x%10 else cur
+            let new_x = x/10
+            mup new_x new_cur
+    mup x 10 
+
+//снизу вверх max
+let mutable a = -1
+let rec max x =
+    if(x=0) then a
+    else
+        if(x%10 > a) then a <- x%10
+        max(x/10)
+//сверху вниз
+let max2 x =
+    let rec pup x cur =
+        if x=0 then cur
+        else    
+            let new_cur = if x%10 > cur then x%10 else cur
+            let new_x = x/10
+            pup new_x new_cur
+    pup x -1
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    Console.WriteLine("Введите число")
+    let x = Console.ReadLine() |> Int32.Parse
+    Console.WriteLine("Произведение цифр (вверх): {0}", mul x)
+    Console.WriteLine("Произведение цифр (вниз): {0}", mul2 x)
+    Console.WriteLine("Минимальная цифра числа (вверх): {0}", min x)
+    Console.WriteLine("Минимальная цифра числа (вниз): {0}", min2 x)
+    Console.WriteLine("Максимальная цифра числа (вверх): {0}", max x)
+    Console.WriteLine("Максимальная цифра числа (вниз): {0}", max2 x)
+    0 
