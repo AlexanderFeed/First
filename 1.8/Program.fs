019be6592a784266a51e0a7e27ae2bc91d0e0f10
Александр Николаевич Мищенко <alex1mishchenko@gmail.com>
2022-03-14 17:36:15 +0300

    Метод 3. Названия могут ввести в заблуждение

--- 1.8/Program.fs
+++ 1.8/Program.fs
@@ -1,6 +1,7 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+let mutable m = -1
 let rec nod a b =
     if a = b then a
     else
@@ -24,30 +25,33 @@ let prost chislo func init =
     let rec schet chislo func init dannoe = 
         if(dannoe <=1) then init
         else
-            let new_init = if nod chislo dannoe <> 1 then  func init dannoe else init
+            let new_init = if nod chislo dannoe = 1 then init 
+                           else 
+                               if (dannoe > init) && ((nod chislo dannoe)%dannoe <> 0) then dannoe else init 
             let new_dannoe = dannoe-1
             schet chislo func new_init new_dannoe
     schet chislo func init chislo
+
+let rec sum x =
+    if x < 5 then x
+    else 
+        if x%10 < 5 then x%10 + sum(x/10)
+        else sum(x/10)
 //сложно
+//let prost_proxod x predicate func init =
+//    let func1 init divider = if predicate divider then func init divider else init
+//    prost x func1 init
+//
+//let nod_proxod x predicate func init =
+//    let func1 init divider = if predicate divider then func init divider else init
+//    vse x func1 init
 
-let Metod1 x predicate func init =
-    let func1 init divider = if predicate divider then func init divider else init
-    prost x func1 init
-
-let nod_proxod x predicate func init =
-    let func1 init divider = if predicate divider then func init divider else init
-    vse x func1 init
-
-let mutable m = -1
-let rec max x =
-        if(x=0) then m
-        else
-            if((x%10 > m) && (x%3 <> 0)) then m <- x%10
-            max(x/10)
 [<EntryPoint>]
 let main argv =
+    Console.WriteLine("НОД 21 7:")
+    Console.WriteLine(nod 21 7)
     Console.WriteLine("Введите число:")
     let x = Console.ReadLine() |> Int32.Parse
-    Console.WriteLine(Metod1 x (fun x -> x%2=0) (fun x y-> x+1) 0)
-
+    //Console.WriteLine(prost (x-1) (fun x -> x%2=0) 0)
+    Console.WriteLine(sum x * prost x (fun x -> x%2 =0) 0)
     0 
