9de833fe93b31fbcb063086a7c49796343135540
Александр Николаевич Мищенко <alex1mishchenko@gmail.com>
2022-03-14 17:57:55 +0300

    Ага

--- 1.9/Program.fs
+++ 1.9/Program.fs
@@ -1,8 +1,59 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+let rec nod a b =
+    if a = b then a
+    else
+        if (a > b) then 
+            let new_a = b 
+            let new_b = a-b
+            nod new_a new_b
+        else 
+            let new_a = a 
+            let new_b = b-a
+            nod new_a new_b
+let vse x func init =
+    let rec vse1 x func init current_divider =
+        if current_divider = 0 then init
+        else
+            let new_init = if x % current_divider = 0 then func init current_divider else init
+            let new_divider = current_divider - 1
+            vse1 x func new_init new_divider
+    vse1 x func init x
+let prost chislo func init :int =
+    let rec schet chislo func init dannoe = 
+        if(dannoe <=1) then init
+        else
+            let new_init = if nod chislo dannoe = 1 then init 
+                           else 
+                               if (dannoe > init) && ((nod chislo dannoe)%dannoe <> 0) then dannoe else init 
+            let new_dannoe = dannoe-1
+            schet chislo func new_init new_dannoe
+    schet chislo func init chislo
+
+let rec sum x =
+    if x < 5 then x
+    else 
+        if x%10 < 5 then x%10 + sum(x/10)
+        else sum(x/10)
+
+let mul (h:int) g : int = 
+    h * g
+//сложно
+//let prost_proxod x predicate func init =
+//    let func1 init divider = if predicate divider then func init divider else init
+//    prost x func1 init
+//
+//let nod_proxod x predicate func init =
+//    let func1 init divider = if predicate divider then func init divider else init
+//    vse x func1 init
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    Console.WriteLine("Введите число:")
+    let x = Console.ReadLine() |> Int32.Parse
+    let b = sum x
+    let c:int = prost x (fun x->x%2=0) 0
+    let a = mul b c
+    Console.WriteLine(a)
+    0 
