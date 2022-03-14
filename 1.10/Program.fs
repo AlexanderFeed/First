dbf4062151f57035055764b2e7ded61e6713723a
Александр Николаевич Мищенко <alex1mishchenko@gmail.com>
2022-03-14 18:16:44 +0300

    Исправляю

--- 1.10/Program.fs
+++ 1.10/Program.fs
@@ -1,8 +1,49 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+let rec nod a b =
+    if a = b then a
+    else
+        if (a > b) then 
+            let new_a = b 
+            let new_b = a-b
+            nod new_a new_b
+        else 
+            let new_a = a 
+            let new_b = b-a
+            nod new_a new_b
+let vse x func init =
+    let rec vse1 x func init current_divider =
+        if current_divider = 0 then init
+        else
+            let new_init = if x % current_divider = 0 then func init current_divider else init
+            let new_divider = current_divider - 1
+            vse1 x func new_init new_divider
+    vse1 x func init x
+
+let prost chislo func init =
+    let rec schet chislo func init dannoe = 
+        if(dannoe <=1) then init
+        else
+            let new_init = if nod chislo dannoe = 1 then func dannoe init else init
+            let new_dannoe = dannoe-1
+            schet chislo func new_init new_dannoe
+    schet chislo func init chislo
+//сложно
+let prost_proxod x predicate func init =
+    let func1 init divider = if predicate divider then func init divider else init
+    prost x func1 init
+
+let nod_proxod x predicate func init =
+    let func1 init divider = if predicate divider then func init divider else init
+    vse x func1 init
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    Console.WriteLine("НОД 21 7:")
+    Console.WriteLine(nod 21 7)
+    Console.WriteLine("Введите число:")
+    let x = Console.ReadLine() |> Int32.Parse
+    Console.WriteLine(prost_proxod x (fun a -> a%2 =0) (fun x y-> x*y) 1)
+    //Console.WriteLine(nod_proxod x (fun a -> a>4) (fun x y -> x+y) 1)
+    0 
