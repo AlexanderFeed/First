f743f3eb48eac7fad200c3c7a63d5c698f8ac87f
Александр Николаевич Мищенко <alex1mishchenko@gmail.com>
2022-03-20 19:19:54 +0300

    Готово

---
+++ 2.2(24)/Program.fs
@@ -0,0 +1,48 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let rec readList n = 
+    if n=0 then []
+    else
+        let Head = Console.ReadLine() |> Convert.ToInt32
+        let Tail = readList (n-1)
+        Head::Tail
+
+let rec writeList = function
+    [] ->   []
+    | (head : int)::tail -> 
+                   System.Console.WriteLine(head)
+                   writeList tail 
+let rec m list mi =
+    match list with
+    |[] -> mi
+    |h::t ->
+            if h> mi then 
+                let new_mi = h 
+                m t new_mi
+            else  m t mi
+
+let rec del list mi new_list =
+    match list with
+    |[] -> new_list
+    |h::t ->
+            if h= mi then 
+                let new_new_list = new_list @ [] 
+                del t mi new_new_list
+            else
+                let new_new_list = new_list@[h]
+                del t mi new_new_list
+
+[<EntryPoint>]
+let main argv =
+    let n = Console.ReadLine() |> Int32.Parse
+    let list = readList n 
+    let first = List.head list
+    let ma1 = m list first
+    let new_list = del list ma1 []
+    let new_first = List.head new_list
+    let ma2 = m new_list new_first
+    Console.WriteLine(ma1)
+    Console.WriteLine(ma2)
+    0 // return an integer exit code
