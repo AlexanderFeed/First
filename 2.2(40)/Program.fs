71a544273150f15b76b3ec607b36865f001f1ae0
Александр Николаевич Мищенко <alex1mishchenko@gmail.com>
2022-03-20 19:51:58 +0300

    Готово

--- 2.2(40)/Program.fs
+++ 2.2(40)/Program.fs
@@ -2,7 +2,34 @@
 
 open System
 
+let rec readlist n =
+    match n with   
+    |0 -> []
+    |_ ->
+        let Head = Console.ReadLine() |> Int32.Parse
+        let Tail = readlist (n-1)
+        Head::Tail
+let rec writelist = function    
+    [] -> []
+    |(h:int)::t ->
+        Console.WriteLine(h)
+        writelist t
+
+let rec minchet list mi func = 
+    match list with
+    |[] -> mi
+    |h::t ->
+            if func mi h then 
+                                let new_mi = h
+                                minchet t new_mi func
+            else
+                minchet t mi func
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let n = Console.ReadLine() |> Int32.Parse
+    let list = readlist n
+    let first = List.head list
+    let a = minchet list first (fun mi h -> h<mi && h%2=0)
+    if(a%2=0) then Console.WriteLine(a)
+    else Console.WriteLine("Нет четных")
     0 // return an integer exit code
