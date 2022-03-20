9fdd7420c3e516ded4ccda7144b9c3e22c48903a
Александр Николаевич Мищенко <alex1mishchenko@gmail.com>
2022-03-21 02:40:53 +0300

    Не доделал

--- 3.1(20)/Program.fs
+++ 3.1(20)/Program.fs
@@ -1,8 +1,45 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿
+
+// 10, 20, 30, 40, 50, 60
 
 open System
 
+let rec readList n = 
+    if n=0 then []
+    else
+        let Head = Console.ReadLine() |> Convert.ToInt32
+        let Tail = readList (n-1)
+        Head::Tail
+
+let rec writeList = function
+    [] ->   []
+    | (head : int)::tail -> 
+                   System.Console.WriteLine(head)
+                   writeList tail 
+             
+let rec listok new_list h t =
+    if(h<t) then 
+                let new_new_list = List.append new_list [h+1]
+                let new_h = h+1
+                listok new_new_list new_h t
+    else 
+        new_list
+
+
+let rec perebor list new_list = 
+    match list with
+    |[] ->new_list
+    |a::b::t ->
+            
+            if(b-a <>1) then 
+                            let new_new_list = listok new_list a (b-1)
+                            perebor t new_new_list
+            else perebor t new_list
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let n = Console.ReadLine() |> Int32.Parse
+    let list = readList n
+    let list = Set.toList(Set.ofList(list))
+    writeList (perebor list [])
     0 // return an integer exit code
