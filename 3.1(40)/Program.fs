7694c76ec1edb8ae6c17fafee6428f95eefe8ebb
Александр Николаевич Мищенко <alex1mishchenko@gmail.com>
2022-03-21 15:50:08 +0300

    Готово

---
+++ 3.1(40)/Program.fs
@@ -0,0 +1,22 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+let rec readList n = 
+    if n=0 then []
+    else
+        let Head = Console.ReadLine() |> Convert.ToInt32
+        let Tail = readList (n-1)
+        Head::Tail
+
+let rec writeList = function
+    [] ->   []
+    | (head : int)::tail -> 
+                   System.Console.WriteLine(head)
+                   writeList tail 
+[<EntryPoint>]
+let main argv =
+    let n = Console.ReadLine() |> Int32.Parse
+    let list = readList n
+    let list = List.filter (fun x -> x%2 =0) list
+    Console.WriteLine(List.min list)
+    0 // return an integer exit code
