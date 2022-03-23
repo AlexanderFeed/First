b0352b2f6fcb7780f29b6ea9f3f669b1122f2254
Александр Николаевич Мищенко <alex1mishchenko@gmail.com>
2022-03-23 20:09:38 +0300

    Метод 2

--- 3.4/Program.fs
+++ 3.4/Program.fs
@@ -11,14 +11,26 @@ let prov str1 str2 =
              else false
     prov1 str1 ((String.length str1)-1) str2 false
 
+let probel str = 
+    let rec probel1 (str:string) n kolvo =
+        match n with
+        |0 -> kolvo
+        |_ ->
+                if str.[n] = ' ' then 
+                                        let new_kolvo = kolvo+1
+                                        probel1 str (n-1) new_kolvo
+                else probel1 str (n-1) kolvo
+    probel1 str ((String.length str)-1) 1
 [<EntryPoint>]
 let main argv =
     let str = Console.ReadLine()
-    let str1 = str.[0..(((String.length str)/2)-1)]
-    if String.length str%2 =0 then 
-                                    let str2 = str.[(((String.length str)/2))..(String.length str)-1]
-                                    Console.WriteLine(prov str1 str2)
-     else 
-        let str2 = str.[(((String.length str)/2)+1)..(String.length str)-1]
-        Console.WriteLine(prov str1 str2)   
+    //let str1 = str.[0..(((String.length str)/2)-1)]
+    //if String.length str%2 =0 then 
+    //                                let str2 = str.[(((String.length str)/2))..(String.length str)-1]
+    //                                Console.WriteLine(prov str1 str2)
+    // else 
+    //    let str2 = str.[(((String.length str)/2)+1)..(String.length str)-1]
+    //    Console.WriteLine(prov str1 str2)   
+    Console.WriteLine(probel str)
+    
     0 // return an integer exit code
