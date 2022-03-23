ec46546583ba5622c35978ed492c6303f00608ba
Александр Николаевич Мищенко <alex1mishchenko@gmail.com>
2022-03-23 21:23:12 +0300

    Метод 3

--- 3.4/Program.fs
+++ 3.4/Program.fs
@@ -4,7 +4,7 @@ open System
 
 let prov str1 str2 =
     let rec prov1 (str1:string) n (str2:string) bol =
-        if n = 0 then bol 
+        if n = -1 then bol 
         else if str1.[n] = str2.[(String.length str1 )-n-1] then 
                                                                 let new_bol = true
                                                                 prov1 str1 (n-1) str2 new_bol
@@ -14,13 +14,42 @@ let prov str1 str2 =
 let probel str = 
     let rec probel1 (str:string) n kolvo =
         match n with
-        |0 -> kolvo
+        |(-1) -> kolvo
         |_ ->
                 if str.[n] = ' ' then 
                                         let new_kolvo = kolvo+1
                                         probel1 str (n-1) new_kolvo
                 else probel1 str (n-1) kolvo
     probel1 str ((String.length str)-1) 1
+
+
+let proxod str elem = 
+    let rec proxod1 (str:string) n elem bol =
+        match n with
+        |(-1) -> bol
+        |_ ->
+                if elem = str.[n] && bol = true then
+                                                    false
+                else if elem = str.[n] && bol = false then 
+                                                            let new_bol = true
+                                                            proxod1 str (n-1) elem new_bol
+                        else proxod1 str (n-1) elem bol
+    proxod1 str ((String.length str)-1) elem false
+let metod3 str = 
+    let rec inmetod3 (str:string) n kolvo =
+        match n with
+        |(-1) -> kolvo
+        |_ -> 
+                if proxod str str.[n] then 
+                                            let new_kolvo = kolvo+1
+                                            inmetod3 str (n-1) new_kolvo
+                else 
+                        let new_kolvo = kolvo+1
+                        let new_str = String.filter (fun x -> x<>str.[n]) str
+                        let otnimaemoe_str = String.filter (fun x -> x=str.[n]) str
+                        inmetod3 new_str (n-(String.length otnimaemoe_str)) new_kolvo
+    inmetod3 str ((String.length str)-1) 0
+        
 [<EntryPoint>]
 let main argv =
     let str = Console.ReadLine()
@@ -31,6 +60,10 @@ let main argv =
     // else 
     //    let str2 = str.[(((String.length str)/2)+1)..(String.length str)-1]
     //    Console.WriteLine(prov str1 str2)   
-    Console.WriteLine(probel str)
+
+
+    //Console.WriteLine(probel str)
     
+
+    Console.WriteLine(metod3 str)
     0 // return an integer exit code
