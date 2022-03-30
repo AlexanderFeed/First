7e23f9943fe8196f07bb1003c67ca601f0e59091
Александр Николаевич Мищенко <alex1mishchenko@gmail.com>
2022-03-30 22:42:44 +0300

    Начал

---
+++ 5Book(LR8)/Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+
+type Maybe<'a> =
+    | Just of 'a
+    | Nothing
+
+let fmapMaybe f p =
+    match p with
+    | Just a -> Just (f a)
+    | Nothing -> Nothing
+
+let applyMaybe lf p =
+    match lf, p with
+    | Just f, Just a -> Just (f a)
+    | _-> Nothing   
+
+let monadMaybe p f =
+    match p with
+    | Just a -> f a
+    | _-> Nothing 
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
