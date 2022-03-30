7cf74084f8923c1daceebb87d3c262af1bf71492
Александр Николаевич Мищенко <alex1mishchenko@gmail.com>
2022-03-30 23:21:26 +0300

    Ну типа

--- 5Book(LR8)/Program.fs
+++ 5Book(LR8)/Program.fs
@@ -19,7 +19,35 @@ let monadMaybe p f =
     | Just a -> f a
     | _-> Nothing 
 
+let id a = a
+let f1 a = a + 3
+let f2 a = a * 5
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    //id не влияет на вычисления
+    let x1 = fmapMaybe f1 (Just 5)
+    Console.WriteLine(id x1)
+    Console.WriteLine(fmapMaybe id x1)
+    // Подьемы композиции = композиция подъемов 
+    let x2 = fmapMaybe f1 x1 
+    let x3 = fmapMaybe f2 x2
+    let x4 = fmapMaybe(f1 >> f2) x1
+    let x5 = applyMaybe (Just f1) (Just 2)
+    Console.WriteLine(x3)
+    Console.WriteLine(x4)
+    //
+    Console.WriteLine(id x5)
+    Console.WriteLine(applyMaybe (Just id) x5)
+    //
+    let a = 2
+    let b = id a
+    let t1 = applyMaybe (Just id) (Just a)
+    let t2 = Just b
+    Console.WriteLine(t1)
+    Console.WriteLine(t2)
+    //ассоциативна
+    //let t1 = applyMaybe (Just f1) (Just 2)
+    //let t2 = applyMaybe (Just 3) (Just f2)//?????????????
+    //Композиция функций apply ассоциативна. не проверить
+    0
