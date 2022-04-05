3532aa8d7111f9b5be103bb9839910eb822a3d0a
Александр Николаевич Мищенко <alex1mishchenko@gmail.com>
2022-04-05 21:46:21 +0300

    Вторая часть шестой лабы

---
+++ 6.2Book(LR8)/Program.fs
@@ -0,0 +1,29 @@
+﻿open System
+
+let num = "5"
+let Gues= MailboxProcessor.Start(fun inbox->
+    let rec messageLoop() = async {
+        let! (msg:string) = inbox.Receive()
+
+        let response = match msg.ToLower() with
+        | "5" -> "Ты угадал загаданное число, ты настоящий уличный маг! Можешь ввести пустую строку и закрыть прогу"
+        |_ -> "Неверно"
+        printfn "<- %s" response
+
+        return! messageLoop()
+    }
+
+    messageLoop()
+)
+
+let rec askUser() =
+    Console.WriteLine("Значит ты уличный маг? Тогда угадай число, котороя я загадал!!!")
+    let input = Console.ReadLine()
+    if not (String.IsNullOrEmpty input) then
+        Gues.Post(input)
+        askUser()
+
+[<EntryPoint>]
+let main argv =
+    askUser()
+    0
